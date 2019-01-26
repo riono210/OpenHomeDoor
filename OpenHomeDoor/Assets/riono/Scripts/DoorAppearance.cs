8d2f7c4d9f2590e2e5beaf080c4174ff879bb786
DESKTOP-RBUMNA9\riono <ryoyassn418@yahoo.co.jp>
2019-01-27 01:00:35 +0900

    床にドア配置

--- OpenHomeDoor/Assets/riono/Scripts/DoorAppearance.cs
+++ OpenHomeDoor/Assets/riono/Scripts/DoorAppearance.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,16 @@ public class DoorAppearance : MonoBehaviour {
 
     public GameObject door;
     public GameObject camera;
+    public GameObject floorParent;
 
-	// Use this for initialization
-	void Start () {
-		
+    private void Awake()
+    {
+        ManagerSinglton.Instance.doorExist.action += DoorCreate;
+    }
+
+    // Use this for initialization
+    void Start () {
+       
 	}
 	
 	// Update is called once per frame
@@ -17,8 +24,30 @@ public class DoorAppearance : MonoBehaviour {
 		
 	}
 
-    public void DoorCreate()
+    public void DoorCreate(bool isdoor)
     {
+        Vector3 doorPos = camera.transform.forward;
+        Vector3 floorPos = Vector3.zero;
+        //Debug.Log("floor;" + floorParent.name);
+        //Debug.Log("floor;" + floorParent.transform.childCount);
 
+        float doorSizeY = door.transform.localScale.y  / 2;
+        var planes = floorParent.GetComponentInChildren<Transform>();
+        foreach(Transform value in planes)
+        {
+
+            SurfacePlane child = value.GetComponent<SurfacePlane>();
+            Debug.Log("scerch..." + child.PlaneType);
+
+            if (child.PlaneType == PlaneTypes.Floor)
+            {
+                floorPos = value.position;
+
+                Debug.Log("find!");
+                break;
+            }
+        }
+        doorPos = new Vector3(doorPos.x, floorPos.y + doorSizeY  , doorPos.z + 1f);
+        Instantiate(door,doorPos, Quaternion.identity);
     }
 }
