85447f0e2bcee3ba28934f5b7b2097cc4ad9bcf4
福田　光希 <e165727@ie.u-ryukyu.ac.jp>
2019-01-27 12:26:36 +0900

    add howto Scene

---
+++ OpenHomeDoor/Assets/Koo/Scripts/goHome.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class goHome : MonoBehaviour {
+
+    private AudioSource decideSound;
+
+	
+	void Start () {
+        decideSound = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        decideSound.PlayOneShot(decideSound.clip);
+        Invoke(("LoadTitle"), 0.7f);
+    }
+
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
