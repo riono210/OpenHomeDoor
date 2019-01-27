433025e2d40c9ceb036038c66aa95d15b2d6ff46
福田　光希 <e165727@ie.u-ryukyu.ac.jp>
2019-01-27 14:43:46 +0900

    result 追加

---
+++ OpenHomeDoor/Assets/Koo/Scripts/canvastext.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvastext : MonoBehaviour {
+
+    private Text scoreText;
+
+    private bool isGameover = false;
+    private bool isClear = false;
+
+    private countTime countTimeScript;
+    private OparateManager oparateManagerScript;
+    public GameObject MainCamera;
+    private GameObject Timer;
+
+	// Use this for initialization
+	void Start () {
+        Timer = GameObject.Find("Timer");
+        countTimeScript = Timer.GetComponent<countTime>();
+        
+        MainCamera = GameObject.Find("MainCamera");
+        oparateManagerScript = MainCamera.GetComponent<OparateManager>();
+
+        scoreText = this.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Timer = GameObject.Find("Timer");
+        countTimeScript = Timer.GetComponent<countTime>();
+
+        isClear = oparateManagerScript.GameClear;
+        isGameover = countTimeScript.gameover;
+
+        if (isClear == true)
+        {
+            scoreText.text = countTimeScript.newseconds.ToString("#");
+        }
+
+        else if (isGameover == true)
+        {
+            scoreText.text = oparateManagerScript.CountDoor.ToString("#");
+        }
+	}
+}
